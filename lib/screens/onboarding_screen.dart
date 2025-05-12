bc39ab58b98d7efa850144c941e373da4f257ca4
amirovsunnat <sunnatbekamirov22@gmail.com>
2025-05-12 21:16:17 +0500

    Splash screen va onboarding screenlari qo'shildi. Va yangi login screeni yaratildi.

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:online_groceries_market/screens/login_screen.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image.asset("assets/images/azizbek.jpg"),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.infinity,
+              height: height * 0.4,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF0E1727).withOpacity(0),
+                    Color(0xFF858585).withOpacity(0.5),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: height * 0.39,
+            child: SvgPicture.asset(
+              "assets/svgs/logo.svg",
+              width: 48,
+              height: 56,
+            ),
+          ),
+          Positioned(
+            bottom: height * 0.23,
+            width: width * 0.62,
+            child: Text(
+              textAlign: TextAlign.center,
+              "Welcome to our store",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontFamily: "Gilroy-Bold",
+                fontSize: 48,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: height * 0.2,
+            child: Text(
+              textAlign: TextAlign.center,
+              "Get your groceries in as fast as one hour",
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.7),
+                letterSpacing: 0.5,
+                fontFamily: "Gilroy-Medium",
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: height * 0.1,
+            left: 30,
+            right: 30,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  CupertinoPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+              child: Container(
+                alignment: Alignment.center,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Color(0xFF53B175),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontFamily: "Gilroy-Regular",
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:online_groceries_market/screens/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF53B175),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 15,
+          children: [
+            SvgPicture.asset("assets/svgs/logo.svg", height: 50, width: 50),
+            Column(
+              spacing: 0,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "nectar",
+                  style: TextStyle(
+                    height: 0,
+                    fontSize: 50,
+                    color: Colors.white,
+                    fontFamily: "Gilroy-Light",
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "online groceriet",
+                  style: TextStyle(
+                    height: -0.5,
+                    fontFamily: "Gilroy-Medium",
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                    letterSpacing: 3,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
