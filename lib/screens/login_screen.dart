2571c6f8e06d5dce10f963d567aed9974ff31afb
lagman5006 <asliddin5006@gmail.com>
2025-05-12 21:30:41 +0500

    Login screeni yozish boshlandi.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,6 +5,12 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("data")
+        ]
+      ),
+    );
   }
 }
