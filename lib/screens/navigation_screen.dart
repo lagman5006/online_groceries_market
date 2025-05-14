767bfb019841fddcf8ffcd5046abb5690953f0b3
amirovsunnat <sunnatbekamirov22@gmail.com>
2025-05-14 22:02:18 +0500

    build profile screen.

--- lib/screens/navigation_screen.dart
+++ lib/screens/navigation_screen.dart
@@ -30,64 +30,92 @@ class _NavigationScreenState extends State<NavigationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: screens[selectectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        onTap: (value) {
-          log(value.toString());
-          setState(() {
-            selectectedIndex = value;
-          });
-        },
-        selectedItemColor: Color(0xFF53B175),
-        unselectedItemColor: Color(0xFF181725),
-        showUnselectedLabels: true,
-        showSelectedLabels: true,
-        currentIndex: selectectedIndex,
-        items: [
-          BottomNavigationBarItem(
-            label: "Shop",
-            icon: SvgPicture.asset(
-              "assets/svgs/home.svg",
-              color:
-                  selectectedIndex == 0 ? Color(0xFF53B175) : Color(0xFF181725),
-            ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(30),
           ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              "assets/svgs/explore.svg",
-              color:
-                  selectectedIndex == 1 ? Color(0xFF53B175) : Color(0xFF181725),
-            ),
+          boxShadow: [
+            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.shifting,
+            onTap: (value) {
+              log(value.toString());
+              setState(() {
+                selectectedIndex = value;
+              });
+            },
+            selectedItemColor: Color(0xFF53B175),
+            unselectedItemColor: Color(0xFF181725),
+            showUnselectedLabels: true,
+            showSelectedLabels: true,
+            currentIndex: selectectedIndex,
+            items: [
+              BottomNavigationBarItem(
+                label: "Shop",
+                icon: SvgPicture.asset(
+                  "assets/svgs/home.svg",
+                  color:
+                      selectectedIndex == 0
+                          ? Color(0xFF53B175)
+                          : Color(0xFF181725),
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/svgs/explore.svg",
+                  color:
+                      selectectedIndex == 1
+                          ? Color(0xFF53B175)
+                          : Color(0xFF181725),
+                ),
 
-            label: "Explore",
-          ),
-          BottomNavigationBarItem(
-            label: "Cart",
+                label: "Explore",
+              ),
+              BottomNavigationBarItem(
+                label: "Cart",
 
-            icon: SvgPicture.asset(
-              "assets/svgs/cart.svg",
+                icon: SvgPicture.asset(
+                  "assets/svgs/cart.svg",
 
-              color:
-                  selectectedIndex == 2 ? Color(0xFF53B175) : Color(0xFF181725),
-            ),
+                  color:
+                      selectectedIndex == 2
+                          ? Color(0xFF53B175)
+                          : Color(0xFF181725),
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/svgs/favorite.svg",
+                  color:
+                      selectectedIndex == 3
+                          ? Color(0xFF53B175)
+                          : Color(0xFF181725),
+                ),
+                label: "Favorite",
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/svgs/profile.svg",
+                  color:
+                      selectectedIndex == 4
+                          ? Color(0xFF53B175)
+                          : Color(0xFF181725),
+                ),
+                label: "Profile",
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              "assets/svgs/favorite.svg",
-              color:
-                  selectectedIndex == 3 ? Color(0xFF53B175) : Color(0xFF181725),
-            ),
-            label: "Favorite",
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              "assets/svgs/profile.svg",
-              color:
-                  selectectedIndex == 4 ? Color(0xFF53B175) : Color(0xFF181725),
-            ),
-            label: "Profile",
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,166 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Profile Screen")));
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            buildProfileHeader(),
+            SizedBox(height: 30),
+            Divider(),
+            buildProfileItem(
+              iconPath: "assets/svgs/orders.svg",
+              text: "Orders",
+              onTap: () {},
+            ),
+            buildProfileItem(
+              iconPath: "assets/svgs/my_details.svg",
+              text: "My Detalis",
+              onTap: () {},
+            ),
+            buildProfileItem(
+              iconPath: "assets/svgs/location.svg",
+              text: "Deliver Address",
+              onTap: () {},
+            ),
+            buildProfileItem(
+              iconPath: "assets/svgs/payment.svg",
+              text: "Payment Methods",
+              onTap: () {},
+            ),
+            buildProfileItem(
+              iconPath: "assets/svgs/ticket.svg",
+              text: "Promo Cord",
+              onTap: () {},
+            ),
+            buildProfileItem(
+              iconPath: "assets/svgs/notification.svg",
+              text: "Notifecations",
+              onTap: () {},
+            ),
+            buildProfileItem(
+              iconPath: "assets/svgs/help.svg",
+              text: "Help",
+              onTap: () {},
+            ),
+            buildProfileItem(
+              iconPath: "assets/svgs/about.svg",
+              text: "About",
+              onTap: () {},
+            ),
+            SizedBox(height: 50),
+            buildLogOutButton(),
+          ],
+        ),
+      ),
+    );
   }
+
+  InkWell buildLogOutButton() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        height: 70,
+        decoration: BoxDecoration(
+          color: Color(0xFFF2F3F2),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          children: [
+            Icon(Icons.logout, color: Color(0xFF53B175), size: 20),
+            SizedBox(width: 110),
+            Text(
+              "Log out",
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: "Gilroy",
+                color: Color(0xFF53B175),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding buildProfileHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 40,
+            child: Image.asset("assets/images/profile.png"),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "Zikrillo",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: "Gilroy",
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit, size: 15, color: Color(0xFF53B175)),
+                  ),
+                ],
+              ),
+              Text(
+                "imomaliyevzikrillo@gmail.com",
+                style: TextStyle(fontSize: 16, color: Color(0xFF7C7C7C)),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+InkWell buildProfileItem({
+  required String iconPath,
+  required String text,
+  required VoidCallback onTap,
+}) {
+  return InkWell(
+    onTap: onTap,
+    child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Row(
+            children: [
+              SvgPicture.asset(iconPath),
+              SizedBox(width: 10),
+              Text(
+                text,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: "Gilroy",
+                  color: Color(0xFF181725),
+                ),
+              ),
+              Spacer(),
+              Icon(Icons.keyboard_arrow_right_outlined),
+            ],
+          ),
+        ),
+        Divider(),
+      ],
+    ),
+  );
 }
